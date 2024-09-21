941d763ba42400be2fe8d6675d70469e12d5f94e
MohdShavezz <mohdpctebtech19cse@gmail.com>
2024-09-21 10:54:20 +0530

    chance in app

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/product/:id" element={<Product />} />
         <Route path="/check" element={<Check />} /> 
-        {/* conflict ke lie main */}
+        {/* conflict ke lie CheckBranch */}
 
       </Routes>
     </Router>
